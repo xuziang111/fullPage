a52ca0c14b0b17c5e076cf7c2a8bc64da78ccc3f
xuziang111 <584788096@qq.com>
2020-04-10 15:17:28 +0800

    组件

---
+++ src/components/fullpage.vue
@@ -0,0 +1,111 @@
+<template>
+<div @wheel="wheelEvent($event)">
+        <transition-group 
+            tag="div" 
+            :name="name">
+                <div class="block" 
+                v-show="index==currentIndex" 
+                v-for="(list,index) in pages" 
+                :key="index" 
+                :style="{'background-color':bgcolor[index]}"
+                @transitionend = "wheelEnd"
+            >{{list}}</div>
+        </transition-group>
+        </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        bgcolor:{
+            type:Array
+        },
+        pages:{
+            type:Number,
+            required:true,
+        }
+    },
+    data(){
+        return{
+    currentIndex:0,
+    name:'',
+    isWheel:true,
+    endEnter:1
+        }
+    },
+    methods:{
+    wheelEvent(e){
+      if(!this.isWheel){
+        return
+      } 
+      if(e.deltaY>0){
+        this.name = 'down'
+        if(this.currentIndex<this.pages-1){
+          this.isWheel =false
+          this.currentIndex++;
+        }
+      }
+      if(e.deltaY<0){
+        this.name = 'up'
+        if(this.currentIndex>0){
+          this.isWheel =false
+          this.currentIndex--;
+        }
+      }
+    },
+    wheelEnd(){
+      this.endEnter++
+      if(this.endEnter == 2){
+        this.endEnter = 0
+        this.isWheel =true
+      }
+    }
+  },
+  mounted(){
+      console.log(this.bgcolor)
+  }
+}
+</script>
+
+<style>
+#fullPageContent{
+    width:100%;
+    height:100%;
+}
+.block{
+        position: absolute;
+        height:100%;
+        width:100%;
+        background:#c99;
+      }
+      .down-enter-active{
+        transition: all 1s ease;
+        transform:translateY(0);
+      }
+      .down-leave-active{
+        transition: all 1s ease;
+        transform:translateY(-100%);
+      }
+      .down-enter{
+        transform:translateY(100%);
+      }
+      .down-leave{
+        transform:translateY(0);
+      }
+
+      .up-enter-active{
+        transition: all 1s ease;
+        transform:translateY(0);
+      }
+      .up-leave-active{
+        transition: all 1s ease;
+        transform:translateY(100%);
+      }
+      .up-enter{
+        transform:translateY(-100%);
+      }
+      .up-leave{
+        transform:translateY(0);
+      }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+import Vue from 'vue/dist/vue.esm.js'
+// import App from './App.vue'
+import Fullpage from '@/components/fullpage.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  components:{
+    Fullpage,
+  },
+  data:{
+    bgcolor:['#222','#333','#444','#555'],
+    pages:4
+  },
+  methods:{
+
+  }
+  
+  // render: h => h(App),
 }).$mount('#app')
